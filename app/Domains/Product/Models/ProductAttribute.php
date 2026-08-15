8917511dc42ab0438e3aa0c3e0c3918dd832d64f
Nganthoiba <leecba@gmail.com>
2026-08-15 21:16:52 +0530

    Product Attribute is related to unit

--- app/Domains/Product/Models/ProductAttribute.php
+++ app/Domains/Product/Models/ProductAttribute.php
@@ -11,9 +11,17 @@ use App\Domains\Master\Models\Organization;
 class ProductAttribute extends Model {
     use BelongsToOrganization;
     use SoftDeletes;
-    protected $fillable = ['organization_id', 'name', 'slug', 'type'];
+    protected $fillable = ['organization_id', 'name', 'slug', 'type', 'unit_id'];
 
     public function organization(): BelongsTo {
         return $this->belongsTo(Organization::class);
+    }
+
+    public function unit(): BelongsTo {
+        return $this->belongsTo(\App\Domains\Master\Models\Unit::class, 'unit_id');
+    }
+
+    public function attributeValues() {
+        return $this->hasMany(ProductAttributeValue::class, 'product_attribute_id');
     }
 }
